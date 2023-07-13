92d5455b888bdb08f7c0a8c7d7ac4ad06e054a24
David Tealdi <tealdidavid12@gmail.com>
2023-07-13 15:19:40 -0300

    develop: version 3.12.0 estable. Se movio de lugar el footer del formDelete y se puso en DeleteTurns

--- src/componets/formDelete/FormDelete.jsx
+++ src/componets/formDelete/FormDelete.jsx
@@ -186,13 +186,7 @@ const Form = () => {
 				</ContenedorBotonCentrado>
 
 			</Formulario>
-
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <div class="col-md-4 d-flex align-items-center">
-                    <span class="mb-3 mb-md-0 text-body-secondary">&copy; 2023 Mediopelo.barbershop</span>
-                </div>
-            </footer>
-            
+			
         </section>
     )
 }
--- src/view/deleteTurns/DeleteTurns.jsx
+++ src/view/deleteTurns/DeleteTurns.jsx
@@ -15,6 +15,12 @@ const DeleteTurnsView = () => {
             <h1 className={style.h2TitleFormDelete}>Eliminar turno</h1>
 
             <DeleteTurns/>
+
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <div class="col-md-4 d-flex align-items-center">
+                    <span class="mb-3 mb-md-0 text-body-secondary">&copy; 2023 Mediopelo.barbershop</span>
+                </div>
+            </footer>
         </>
     )
 }
